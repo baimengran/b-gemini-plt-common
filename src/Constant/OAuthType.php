d75a332b6508c88ace2878b9be15ffb5f7c2ae3b
AMA <450175191@qq.com>
2024-03-21 17:11:49 +0800

    增加获取用户ID

---
+++ src/Constant/OAuthType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GeminiD\PltCommon\Constant;
+
+enum OAuthType:int
+{
+    /**
+     * @var int 微信小程序
+     */
+    case WECHAT_MINI_APP = 0;
+}
--- src/RPC/User/UserInterface.php
+++ src/RPC/User/UserInterface.php
@@ -2,10 +2,14 @@
 
 namespace GeminiD\PltCommon\RPC\User;
 
+use GeminiD\PltCommon\Constant\OAuthType;
+
 interface UserInterface
 {
     public const NAME = 'UserUserInterface';
-    public function ping():bool;
 
+    public function ping(): bool;
+
+    public function firstByCode(string $code, string $appid, int|OAuthType $type = OAuthType::WECHAT_MINI_APP);
 
 }
